4cc933beda92c71b84e89930213b15a6c512f5a4
fanny marcel <marcelf@marcelf-OptiPlex-760.(none)>
2012-05-10 11:53:52 +0200

    ajout du bind

--- app/views/user/index.php
+++ app/views/user/index.php
@@ -9,6 +9,7 @@
             .tablesorterPager({container: $("#pager")});
 
         $('.editQuota').bind('click',function(){
+            alert('on a clique');
             idUser = $(this).parent().attr('id');
             htmlVal = $("#"+idUser+" .inputQuota").html();
             $("#"+idUser+" .inputQuota").wrapInner(document.createElement("input"));
